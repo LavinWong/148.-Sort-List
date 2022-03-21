12f9b7bb4568eea3f31733f6944ef841e9ad37e9
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-03-21 14:03:49 -0400

    Create sort_list.py

---
+++ sort_list.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def sortList(self, head):
+        # if list if empty or catains just one element
+        if not head or not head.next:
+            return head
+        
+        #Split the list
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left,right)
+
+
+    def getMid(self,head):
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        tail = dummy = ListNode()
+
+        while left and right:
+            if left.val<right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+
+            tail = tail.next
+        
+        if left:
+            tail.next = left
+        
+        if right:
+            tail.next = right
+        
+        return dummy.next
